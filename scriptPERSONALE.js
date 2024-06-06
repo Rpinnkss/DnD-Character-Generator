8604e1f0ebf6ddc0ecb3f978de82566db1b30e0e
Rpinnkss <francesco.gaggiano@marconicloud.it>
2024-06-06 05:50:45 +0200

    modify

--- scriptPERSONALE.js
+++ scriptPERSONALE.js
@@ -14,30 +14,50 @@ function generaNumeroCasuale(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+var nomiUtilizzati = [];
+
 function generaPersonaggio() {
-  caricaDati('data/class/index.json', function(response) {
-      var classiFiles = JSON.parse(response);
-      var classiKeys = Object.keys(classiFiles);
-      var fileCasuale = classiFiles[classiKeys[generaNumeroCasuale(0, classiKeys.length - 1)]];
+    caricaDati('data/class/index.json', function(response) {
+        var classiFiles = JSON.parse(response);
+        var classiKeys = Object.keys(classiFiles);
+        var fileCasuale = classiFiles[classiKeys[generaNumeroCasuale(0, classiKeys.length - 1)]];
 
-      caricaDati('data/class/' + fileCasuale, function(response) {
-          var classe = JSON.parse(response).class;
-          var nomiClasse = classe.map(function(classe) {
-              return classe.name;
-          });
-          var classeCasuale = nomiClasse[generaNumeroCasuale(0, nomiClasse.length - 1)];
+        caricaDati('data/class/' + fileCasuale, function(response) {
+            var classe = JSON.parse(response).class;
+            var nomiClasse = classe.map(function(classe) {
+                return classe.name;
+            });
+            var classeCasuale = nomiClasse[generaNumeroCasuale(0, nomiClasse.length - 1)];
 
-          caricaDati('data/races.json', function(response) {
-              var razze = JSON.parse(response).race;
-              var nomiRazze = razze.map(function(razza) {
-                  return razza.name;
-              });
-              var razzaCasuale = nomiRazze[generaNumeroCasuale(0, nomiRazze.length - 1)];
+            caricaDati('data/races.json', function(response) {
+                var razze = JSON.parse(response).race;
+                var nomiRazze = razze.map(function(razza) {
+                    return razza.name;
+                });
+                var razzaCasuale = nomiRazze[generaNumeroCasuale(0, nomiRazze.length - 1)];
 
-                      var risultato = document.getElementById("risultato");
-                      risultato.innerHTML = "<p>Class: " + classeCasuale + "</p>" +
-                          "<p>Race: " + razzaCasuale + "</p>";
-          });
-      });
-  });
+                // Carica i nomi dei personaggi
+                caricaDati('data/nomi.json', function(response) {
+                    var nomi = JSON.parse(response).names;
+                    var nomeCasuale = generaNomeCasuale(nomi);
+
+                    var risultato = document.getElementById("risultato");
+                    risultato.innerHTML = "<p>Name: " + nomeCasuale + "</p>" +
+                        "<p>Class: " + classeCasuale + "</p>" +
+                        "<p>Race: " + razzaCasuale + "</p>";
+                });
+            });
+        });
+    });
+}
+
+function generaNomeCasuale(nomi) {
+    var nomeCasuale = nomi[generaNumeroCasuale(0, nomi.length - 1)];
+    // Controlla se il nome è già stato utilizzato, se sì, genera un nuovo nome
+    if (nomiUtilizzati.includes(nomeCasuale)) {
+        return generaNomeCasuale(nomi);
+    } else {
+        nomiUtilizzati.push(nomeCasuale);
+        return nomeCasuale;
+    }
 }
